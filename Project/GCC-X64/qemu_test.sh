3470daf9822edc8318a7d3328abd1034f1559a49
华楷文 <kevin@Kevin-de-MacBook-Pro.local>
2025-05-25 00:26:16 +0800

    ⚡Let's GO!️

--- Project/GCC-X64/qemu_test.sh
+++ Project/GCC-X64/qemu_test.sh
@@ -1,2 +1,2 @@
 # typical desktop system, 1 numa node, 1 socket, 1 processors, 4GB RAM */
-qemu-system-x86_64 -serial stdio -net none -smp 1 -cpu IvyBridge -m 4096 -cdrom ./RME.iso -monitor none -D qemu-log.txt
+qemu-system-x86_64 -serial stdio -net none -smp 1 -cpu IvyBridge -m 1024 -cdrom ./RME.iso -monitor none -D qemu-log.txt -nographic
